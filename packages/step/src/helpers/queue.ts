b65fa4882ce536a73757a31519370226f24f8ff5
Achintha Weerasinghe <achinthamadumal@gmail.com>
2024-07-19 17:12:21 +0530

    docs added to queue class

--- packages/step/src/helpers/queue.ts
+++ packages/step/src/helpers/queue.ts
@@ -1,14 +1,32 @@
+/**
+ * Represents a priority queue data structure.
+ * Elements can be enqueued with a priority and dequeued based on their priority.
+ */
 export class PriorityQueue<T = any> {
   private _queue: [number, T][] = [];
 
+  /**
+   * Check if the queue is empty.
+   */
   get isEmpty() {
     return this.size === 0;
   }
 
+  /**
+   * Returns the size of the queue.
+   */
   get size() {
     return this._queue.length;
   }
 
+  /**
+   * Adds one or more items to the priority queue with the specified priority.
+   * Items are added based on the specified priority and then sorted in descending order of priority.
+   * Complexity: O(n)
+   *
+   * @param item - The item or items to be added to the queue.
+   * @param p - The priority of the item(s) being added (default is 0).
+   */
   enqueue(item: T | T[], p: number = 0): void {
     if (!Array.isArray(item)) {
       item = [item];
@@ -18,6 +36,16 @@ export class PriorityQueue<T = any> {
     this._queue.sort((a, b) => b[0] - a[0]);
   }
 
+  /**
+   * Removes and returns one or more items from the priority queue based on their priority.
+   * If 'inParallel' is true, returns all items with the same highest priority.
+   * If 'inParallel' is false, returns only one item with the highest priority.
+   * Throws an error if the queue is empty.
+   * Complexity: O(k) - k is the number of items returning
+   *
+   * @param inParallel - Indicates whether to dequeue items in parallel (default is true).
+   * @returns The dequeued item or items based on the specified conditions.
+   */
   dequeue(inParallel: boolean = true): T | T[] {
     if (this.isEmpty) {
       throw new Error('No elements in the queue!');
@@ -36,6 +64,13 @@ export class PriorityQueue<T = any> {
     return items;
   }
 
+  /**
+   * Retrieves the element with the highest priority from the priority queue without removing it.
+   * Returns the element if the queue is not empty, otherwise returns undefined.
+   * Complexity: O(1)
+   *
+   * @returns The element with the highest priority or undefined if the queue is empty.
+   */
   peak(): T | undefined {
     const item = this._peak();
 
